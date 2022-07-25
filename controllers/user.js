6409020afe65f4b7761e9a3bd54935acd0c0a941
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-25 14:33:22 -0400

    userGet with Promise.all

--- controllers/user.js
+++ controllers/user.js
@@ -9,11 +9,19 @@ const userGet = async (req = request, res = response) => {
     const { limit = 5, from = 0 } = req.query;
     const query = { state: true };
 
-    const users = await User.find(query)
-        .skip(Number(from))
-        .limit(Number(limit));
+    // const users = await User.find(query)
+    //     .skip(Number(from))
+    //     .limit(Number(limit));
 
-    const total = await User.countDocuments(query);
+    // const total = await User.countDocuments(query);
+
+    const response = await Promise.all([
+        User.countDocuments(query),
+        User.find(query)
+            .skip(Number(from))
+            .limit(Number(limit))
+
+    ]);
 
     res.json({
         total,
