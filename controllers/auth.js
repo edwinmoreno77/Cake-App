d2d4acf94eed6dcd70e3e4814f58607320f033cb
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-27 16:30:20 -0400

    JWT was created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,56 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/user');
+
+const { generateJWT } = require('../helpers/generate-jwt');
 
 
 const login = async (req, res = response) => {
 
-    res.json({
-        message: 'login ok'
-    });
+    const { email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'User does not exist - email'
+            });
+        }
+
+        // Validate if user is active
+        if (!user.state) {
+            return res.status(400).json({
+                msg: 'User does not exist - state: false'
+            });
+        }
+
+
+        // Validate password
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'User/password incorrect - password'
+            });
+        }
+
+
+        // Create JWT Payload
+        const token = await generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'contact admin'
+        });
+    }
+
+
 }
 
-
-module.exports = login;
+module.exports = login
