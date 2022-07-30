c17d636df85f48d1df1d8835489f8060b64d2c04
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-30 13:18:17 -0400

    validateFields was added to the index

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,9 +4,11 @@
 const isValidRole = require('../helpers/db-validators');
 const validateJWT = require('../middlewares/validate-jwt');
 const validateRole = require('../middlewares/validate-roles');
+const validateFields = require('../middlewares/validate-fields');
 
 module.exports = {
     ...isValidRole,
     ...validateJWT,
-    ...validateRole
+    ...validateRole,
+    ...validateFields
 }
