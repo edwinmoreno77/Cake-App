7566ad8796a99285646aa82d11320a1105b7643c
Edwin Moreno <edwinmoreno77@gmail.com>
2022-07-30 13:22:45 -0400

    index was created

---
+++ models/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const Categorie = require('./categorie');
+const Role = require('./role');
+const Server = require('./server');
+const User = require('./user');
+
+module.exports = {
+    Categorie,
+    Role,
+    Server,
+    User
+}
+
