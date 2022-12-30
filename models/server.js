a2f6d4c40ad73c842df280190f64e1bdc5a308c2
Edwin Moreno <edwinmoreno77@gmail.com>
2022-12-30 16:24:30 -0400

    update public file

--- models/server.js
+++ models/server.js
@@ -6,13 +6,13 @@ const { dbConnection } = require('../database/config.db');
 
 // CORSOPTIONS ME PERMITE DEFINIR DE QUE ORIGEN VAN A VENIR LAS PETICIONES
 
-const corsOptions = {
-    // origin: 'http://localhost:5173',
-    origin: 'https://basic-backend-ready.vercel.app',
-    // origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
+// const corsOptions = {
+//     // origin: 'http://localhost:5173',
+//     origin: 'https://basic-backend-ready.vercel.app',
+//     // origin: '*',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
 
 class Server {
 
@@ -44,10 +44,10 @@ class Server {
 
     middlewares() {
         //CORS
-        // this.app.use(cors());
+        this.app.use(cors());
 
         // PARA CONFIGURAR EL CORS OPTIONS
-        this.app.use(cors(corsOptions));
+        // this.app.use(cors(corsOptions));
 
         //READ AND PARSE JSON
         this.app.use(express.json());
@@ -74,9 +74,9 @@ class Server {
         this.app.use(this.paths.uploads, require('../routes/uploads'));
 
         // ESTA RUTA LE DICE A EXPRESS QUE CUALQUIER RUTA QUE NO ESTE DEFINIDA EN LAS RUTAS ANTERIORES, ME DEVUELVA EL INDEX.HTML Y REACT SE ENCARGUE DE MANEJAR LAS RUTAS
-        this.app.get('*', (req, res) => {
-            res.sendFile('index.html', { root: 'public' });
-        });
+        // this.app.get('*', (req, res) => {
+        //     res.sendFile('index.html', { root: 'public' });
+        // });
     }
 
 
